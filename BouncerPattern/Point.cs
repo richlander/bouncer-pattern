afac19f8e89cd2320dbe8fad430125593c5c3640
Rich Lander <rlander@microsoft.com>
2016-10-27 23:17:23 -0700

    Initial repo

---
+++ BouncerPattern/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Point
+{
+    public int X;
+    public int Y;
+
+    public void GetCoordinates (out int x, out int y)
+    {
+        x = 0;
+        y = 0;
+    }
+}
---
+++ BouncerPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+class BouncerPattern
+{
+    static void Method(object o)
+    {
+        if (o is null) throw new ArgumentException(nameof(o));
+        if (o is Point p)
+        {
+            // implement method here
+        }
+        else throw new ArgumentException("Not a Point", nameof(p));
+    }
+}
